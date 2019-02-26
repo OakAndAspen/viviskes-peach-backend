c90ceeff08427c049ba6b46439ce205351e6b62d
Irina Despot <irinadespot@gmail.com>
2019-02-26 15:12:01 +0100

    User entity

---
+++ src/Entity/User.php
@@ -0,0 +1,250 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=1000)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isAdmin;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isActive;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $celticName;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $phone;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $address;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $npa;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $city;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\User", inversedBy="newbie", cascade={"persist", "remove"})
+     */
+    private $mentor;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\User", mappedBy="mentor", cascade={"persist", "remove"})
+     */
+    private $newbie;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getIsAdmin(): ?bool
+    {
+        return $this->isAdmin;
+    }
+
+    public function setIsAdmin(bool $isAdmin): self
+    {
+        $this->isAdmin = $isAdmin;
+
+        return $this;
+    }
+
+    public function getIsActive(): ?bool
+    {
+        return $this->isActive;
+    }
+
+    public function setIsActive(bool $isActive): self
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getCelticName(): ?string
+    {
+        return $this->celticName;
+    }
+
+    public function setCelticName(?string $celticName): self
+    {
+        $this->celticName = $celticName;
+
+        return $this;
+    }
+
+    public function getPhone(): ?string
+    {
+        return $this->phone;
+    }
+
+    public function setPhone(?string $phone): self
+    {
+        $this->phone = $phone;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(?string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getNpa(): ?int
+    {
+        return $this->npa;
+    }
+
+    public function setNpa(?int $npa): self
+    {
+        $this->npa = $npa;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(?string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getMentor(): ?self
+    {
+        return $this->mentor;
+    }
+
+    public function setMentor(?self $mentor): self
+    {
+        $this->mentor = $mentor;
+
+        return $this;
+    }
+
+    public function getNewbie(): ?self
+    {
+        return $this->newbie;
+    }
+
+    public function setNewbie(?self $newbie): self
+    {
+        $this->newbie = $newbie;
+
+        // set (or unset) the owning side of the relation if necessary
+        $newMentor = $newbie === null ? null : $this;
+        if ($newMentor !== $newbie->getMentor()) {
+            $newbie->setMentor($newMentor);
+        }
+
+        return $this;
+    }
+}
