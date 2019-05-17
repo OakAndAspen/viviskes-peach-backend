35955235a12932e81ae5b993e9d710e2f8a533de
Irina Despot <irinadespot@gmail.com>
2019-05-17 11:19:20 +0200

    File upload

---
+++ src/Controller/FileController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Service\UtilityService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\JsonResponse as JR;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/files")
+ */
+class FileController extends AbstractController implements TokenAuthenticatedController
+{
+    /**
+     * @Route("", name="files-upload", methods={"POST"})
+     *
+     * @param Request $req
+     * @param EntityManagerInterface $em
+     * @return JR
+     */
+    public function upload(Request $req, EntityManagerInterface $em)
+    {
+        $name = $req->get("name");
+        $file = $req->files->get("file");
+        $url = "uploads\\" . $name;
+        move_uploaded_file($file, $url);
+        return new JsonResponse(["url" => $url]);
+    }
+}
